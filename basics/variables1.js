e6ca9b47f5c85447faf7646b751c550c7ebc7b06
Apurv445 <sharmabipin0951@gmail.com>
2025-05-28 09:02:17 +0000

    container config done

---
+++ basics/variables1.js
@@ -0,0 +1,14 @@
+const accountId = 12456 ;
+let accountEmail = "Apurv@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+// accountId = 2 ; //not allowed
+
+ accountEmail = "bhulan@gmail.com";
+ accountPassword = "1289565";
+ accountCity = "Ranchi";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
